5517a27699680c9c45d11b76298a52fe2f604bcb
serhatprogramming <serhatprogramming@gmail.com>
2023-08-26 15:54:43 -0400

    no functionality

---
+++ src/components/ResultDisplay.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ResultDisplay = () => {
+  const yay = 0;
+  const nay = 0;
+  const neutral = 0;
+  return (
+    <div>
+      <hr />
+      <div>
+        Yay: {yay} Nay: {nay} Neutral: {neutral}
+      </div>
+    </div>
+  );
+};
+
+export default ResultDisplay;
---
+++ src/components/Vote.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Vote = () => {
+  return (
+    <div>
+      <button>Yay</button>
+      <button>Nay</button>
+      <button>Neutral</button>
+    </div>
+  );
+};
+
+export default Vote;
